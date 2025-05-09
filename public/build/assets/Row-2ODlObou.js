e95e4dec584d236ae5c3d1a57f4cdf6fdae175d6
a-h-a-m <ahasanmubarok@gmail.com>
2025-05-10 05:43:38 +0700

    fix link unduh

--- public/build/assets/Row-2ODlObou.js
+++
@@ -1,1 +0,0 @@
-import{R as r}from"./app-CJkt09Ir.js";function a({children:e}){return r.createElement("div",{className:"row"},e)}export{a as R};
---
+++ public/build/assets/Row-DypNBgcS.js
@@ -0,0 +1,1 @@
+import{R as r}from"./app-p2sGRXNR.js";function a({children:e}){return r.createElement("div",{className:"row"},e)}export{a as R};
